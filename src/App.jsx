4a21b5ba47b6a947b3784fe48f0cf40e1e1d2be7
Faisal Shohag <50203997+faisal-shohag@users.noreply.github.com>
2024-03-18 12:25:47 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ function App() {
      <Toaster/>
       <Header/>
       <Banner/>
-      <SectionTitle title='Our Recipes' subtitle='Embark on a culinary adventure with our curated collection of delightful dishes. From comforting classics to innovative creations, discover inspiration for every palate and occasion.'/>
+      <SectionTitle title='Our Recipes' subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere pretium metus at feugiat. Fusce at luctus purus. Nunc pharetra gravida ex a scelerisque. Aliquam pharetra condimentum euismod. Duis scelerisque turpis in erat molestie, a viverra urna sollicitudin. Proin eget elementum elit, id euismod ex. Nulla dictum finibus elit dictum sollicitudin. Etiam velit erat, blandit vitae sagittis a, tempor eget quam. Integer mauris augue, dictum tincidunt lectus eu, commodo molestie mi. Integer tincidunt risus nibh, id egestas erat suscipit et. Vestibulum ut arcu placerat, congue dui sit amet, condimentum purus. Ut eget vulputate nisi, quis posuere enim. Donec eleifend massa ut erat malesuada posuere. Mauris ipsum erat, tempor eu auctor eu, convallis quis felis.'/>
       <div className='flex lg:flex-row flex-col gap-5'>
         <div className='flex-1'><Posts handleCook={handleCook} cooks={cooks}/></div>
         <div className='lg:w-[40%]' ><div className="border-[1px] rounded-xl py-5">
